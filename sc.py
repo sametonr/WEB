6d37faa9ed20ee3995c31898609151ac15f6d2a7
sametonr <75686345+sametonr@users.noreply.github.com>
2024-03-01 16:51:30 +0300

    Update sc.py

--- sc.py
+++ sc.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
+import socket
+import time
 
-import requests
-import concurrent.futures
-
-def send_data(target_url):
-    data = "x" * 1024  # Gönderilecek veri, 1024 byte uzunlugunda "x" karakterleri
-    try:
-        response = requests.post(target_url, data=data)
-        print(f"Response: {response.status_code}")
-    except Exception as e:
-        print(f"Error: {e}")
+def send_udp_packet(target_host, target_port, num_packets, packets_per_second, duration):
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    interval = 1 / packets_per_second  # Saniyedeki paket sayisina karsilik gelen aralik
+    start_time = time.time()
+    sent_packets = 0
+    while True:
+        if sent_packets >= num_packets:
+            break
+        if time.time() - start_time > duration:
+            break
+        udp_socket.sendto(b"UDP Test Packet", (target_host, target_port))
+        sent_packets += 1
+        time.sleep(interval)  # Belirlenen aralik kadar bekleyin
+    udp_socket.close()
 
 if __name__ == "__main__":
-    target_url = input("Hedef URL'yi girin: ")
-    num_requests = 1000000  # 1M veri gönderilecek
+    target_host = input("Hedef IP adresini girin: ")
+    target_port = int(input("Hedef port numarasini girin: "))
+    num_packets = int(input("Gonderilecek toplam paket sayisini girin: "))
+    packets_per_second = int(input("Saniyede gonderilecek paket sayisini girin: "))
+    duration = int(input("Devam edecek sureyi saniye cinsinden girin: "))
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_requests) as executor:
-        futures = [executor.submit(send_data, target_url) for _ in range(num_requests)]
-
-        for future in concurrent.futures.as_completed(futures):
-            future.result()
-
-    print("SUCCES")
+    send_udp_packet(target_host, target_port, num_packets, packets_per_second, duration)

